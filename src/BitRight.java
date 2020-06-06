fb8d04f12268732c9a38d3214ad801f03b2da98d
Temzor <onewaykd@gmail.com>
2020-06-06 19:12:25 +0300

    Initial commit

---
+++ src/BitRight.java
@@ -0,0 +1,9 @@
+public class BitRight {
+    public static void main(String args[]){
+        int a = 32;
+        a = a >> 2;
+        System.out.println("a >> 2 = " + a);
+        a = a << 300;
+        System.out.println("a << 2 = " + a);
+    }
+}
